20ba964b6820d47930421cfdfd332cd9ecb89a49
Colson Scott <1670@holbertonschool.com>
2020-02-06 16:17:25 +0000

    Added solution for exercise 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase followed by uppercase
+ * Return: 0 if no errors
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
