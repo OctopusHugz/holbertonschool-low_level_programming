15d5370daf64dbebe37657869126f81ad9e642f0
Colson Scott <1670@holbertonschool.com>
2020-02-06 16:24:00 +0000

    Added solution for exercise 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase (except for q and e)
+ * Return: 0 if no errors
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		if (x == 'e' || x == 'q')
+		{
+			x++;
+		}
+		else
+		{
+			putchar(x);
+		}
+	putchar('\n');
+	return (0);
+}
