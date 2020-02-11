a3ec5a2a9c471f2c401294ccde3cd4751046fe8d
Colson Scott <colsonscott53@gmail.com>
2020-02-11 15:14:26 -0600

    Fixed formatting for exercise 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * print_to_98 - prints all natural numbers from n to 98, followed by new line
  * @n: integer passed to function as argument
  *
  */
