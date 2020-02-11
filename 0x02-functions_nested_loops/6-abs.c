ca653b78583bfd72f1de5e2bbc7f6a013cbe6cb8
Colson Scott <colsonscott53@gmail.com>
2020-02-11 09:57:37 -0600

    Fixed solution for exercise 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 
 /**
- * main - no main just function definition
+ * _abs - returns absolute value of an integer
+ * @n: integer passed to the function as an argument
  *
  * Return: 0 if success
  */
 
 int _abs(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
 		return (-n);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		return (0);
 	}
