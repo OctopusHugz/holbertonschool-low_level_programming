59edd5099c1213a2c33537354dea708943166811
Colson Scott <colsonscott53@gmail.com>
2020-02-10 20:16:00 -0600

    Fixed solution for exercise 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,21 +6,16 @@
  * Return: 0 if success
  */
 
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
 void print_alphabet_x10(void)
 {
 	int x, y;
 
 	for(x = 0; x < 10; x++)
 	{
-		for(y = 'a'; y < 'z'; y++)
+		for(y = 'a'; y <= 'z'; y++)
 		{
 			_putchar(y);
 		}
+		_putchar('\n');
 	}
 }
