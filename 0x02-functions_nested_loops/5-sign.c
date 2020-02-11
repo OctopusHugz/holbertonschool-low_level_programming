ea24a1eb6a44248bef4cda0e7dee663f6b01ddea
Colson Scott <colsonscott53@gmail.com>
2020-02-11 09:55:33 -0600

    Fixed solution for exercise 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - no main just function definition
+ * print_sign - no main just function definition
+ * @n: character passed to function as an argument
  *
  * Return: 0 if success
  */
 
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
