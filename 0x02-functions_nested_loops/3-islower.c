7c7893fadfb5d616e04cf24ea092a83b1f3453f9
Colson Scott <colsonscott53@gmail.com>
2020-02-11 09:43:40 -0600

    Fixed solution for exercise 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * main - checks for a lowercase character
+ * _islower - checks for a lowercase character and returns 1 if true, 0 if not
  *
  * Return: 0 if success
  */
 
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
