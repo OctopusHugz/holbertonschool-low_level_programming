8f64f007402404a61a6739f93cb9c6aceb0a9235
Colson Scott <colsonscott53@gmail.com>
2020-02-11 15:13:01 -0600

    Added solution for exercise 11 and updated holberton header file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * @n: integer passed to function as argument
+ *
+ */
+
+void print_to_98(int n)
+{
+	int x;
+
+	if (n <= 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (x == 98)
+			{
+				printf("%d", x);
+			}
+			else
+			{
+				printf("%d, ", x);
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (x = n; x >= 98; x--)
+		{
+			if (x == 98)
+			{
+				printf("%d", x);
+			}
+			else
+			{
+				printf("%d, ", x);
+			}
+		}
+		printf("\n");
+	}
+}
