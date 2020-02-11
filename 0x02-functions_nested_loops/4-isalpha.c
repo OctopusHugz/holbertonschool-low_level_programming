cb3fd9f5ffeb0fc1695dedefb64be45a540ac4c0
Colson Scott <colsonscott53@gmail.com>
2020-02-11 09:53:42 -0600

    Fixed solution for exercise 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - no main just function definition
+ * _isalpha - no main just function definition
+ * @c: character passed to function as an argument
  *
  * Return: 0 if success
  */
 
 int _isalpha(int c)
 {
-	if(c >= 'A' && c <= 'z')
+	if (c >= 'A' && c <= 'z')
 	{
 		return (1);
 	}
