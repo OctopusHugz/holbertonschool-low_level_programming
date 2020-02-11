09f01b3fc342b1a311aef3774f575016b3574b27
Colson Scott <colsonscott53@gmail.com>
2020-02-11 14:58:12 -0600

    Added solution for exercise 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - adds 2 numbers together
+ * @a: first number passed as argument
+ * @b: second number passed as argument
+ *
+ * Return: 0 if success
+ */
+
+int add(int a, int b)
+{
+	int r;
+
+	r = (a + b);
+	return (r);
+}
