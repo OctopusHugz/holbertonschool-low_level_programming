287dc709a45fa167007ff7b6df9dc42dd1c31edd
Colson Scott <colsonscott53@gmail.com>
2020-02-11 11:15:59 -0600

    Added solution for exercise 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * 24_hours - prints every minute and every hour of an entire day
+ *
+ * Return: 0 if success
+ */
+
+void jack_bauer(void)
+{
+	int w, x, y, z;
+
+	for (w = '0'; w <= '2'; w++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			for (y = '0'; y <= '5'; y++)
+			{
+				for (z = '0'; z <= '9'; z++)
+				{
+					if (w == '2' && x >= '4')
+					{
+						return;
+					}
+					else
+					{
+						_putchar(w);
+						_putchar(x);
+						_putchar(':');
+						_putchar(y);
+						_putchar(z);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
