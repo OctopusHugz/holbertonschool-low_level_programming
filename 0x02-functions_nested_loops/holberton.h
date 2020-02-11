56302b1afd9950de6a1cdaccfa745082dc300627
Colson Scott <colsonscott53@gmail.com>
2020-02-10 20:16:39 -0600

    Added Holberton header file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/**
+ * main - no main since header file, function prototypes only
+ * Return: no return value
+ */
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+#endif
