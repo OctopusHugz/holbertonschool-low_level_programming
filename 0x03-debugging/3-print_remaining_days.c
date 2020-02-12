94c1901d1a5cad5b2f2dcc2354047e478f9a95bf
Colson Scott <colsonscott53@gmail.com>
2020-02-11 20:29:13 -0600

    Fixed solution for exercise 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,7 +18,6 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -30,11 +29,14 @@ void print_remaining_days(int month, int day, int year)
 			{
 				day++;
 			}
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+			}
 		}
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 	else
