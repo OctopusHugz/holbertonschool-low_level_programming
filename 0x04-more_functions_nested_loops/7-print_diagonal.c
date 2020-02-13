bd05b6ecc78beae8a8bc37f17a4028014caea083
Colson Scott <colsonscott53@gmail.com>
2020-02-13 14:53:41 -0600

    Added solution for exercise 7 and updated holberton header file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of spaces to put before diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = i + 1; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
