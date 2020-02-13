da862f87738632d5ba3e576ce10ae3af811490d8
Colson Scott <colsonscott53@gmail.com>
2020-02-13 13:47:41 -0600

    Fixed formatting for exercise 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,11 +2,13 @@
 
 /**
  * print_line - prints a straight line of length n in the terminal
+ * @n: integer passed to function as argument, length of line to print
  */
 
 void print_line(int n)
 {
 	int i;
+
 	if (n != 0)
 	{
 		for (i = 0; i < n; i++)
