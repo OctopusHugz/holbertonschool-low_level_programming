71d6f53d90708dac52cf97f0e1972b49144db5e8
Colson Scott <colsonscott53@gmail.com>
2020-02-13 10:04:40 -0600

    Added solution for exercise 2 and updated holberton header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer passed as argument to function to multiply
+ * @b: second integer passed as argument to funciton to multiply
+ *
+ * Return: product of multiplying the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
