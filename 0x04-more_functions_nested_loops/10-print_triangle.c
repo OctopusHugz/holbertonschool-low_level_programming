6c1238b540074efabc11efeb2ed842ecd58eb703
Colson Scott <colsonscott53@gmail.com>
2020-02-13 22:17:06 -0600

    Added solution for exercise 10 and updated holberton header file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle of the specified size
+ * @size: integer passed to function to specify size of triangles
+ */
+
+void print_triangle(int size)
+{
+	int w, x, y, z;
+
+	z = size;
+	if (size > 0)
+	{
+		for (w = 0; w < size; w++)
+		{
+			for (x = 1; x < z; x++)
+			{
+				_putchar(' ');
+			}
+			for (y = 1; y <= z; y++)
+			{
+				_putchar('#');
+			}
+			//_putchar('\n');
+			z--;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
