6e06ba9b1474a17674e269a7026584d526c2608f
Colson Scott <colsonscott53@gmail.com>
2020-02-13 21:04:27 -0600

    Added solution for exercise 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_square - prints a square with paraeter size as dimensions
+ * @size: integer passed to function to specify size of square to print
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
