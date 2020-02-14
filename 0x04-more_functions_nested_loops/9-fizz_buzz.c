d503a9cc225c749d1140d69d741fb81d3d127747
Colson Scott <colsonscott53@gmail.com>
2020-02-13 21:18:28 -0600

    Added solution for exercise 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - prints the numbers from 1 to 100, printing Fizz if number
+ * is divisible by 3 and printing Buzz if number is divisible by 5
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+}
