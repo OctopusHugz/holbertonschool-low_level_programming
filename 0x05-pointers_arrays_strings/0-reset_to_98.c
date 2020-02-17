c23c648417cb655f78e5522ab05601ecaec3208a
Colson Scott <1670@holbertonschool.com>
2020-02-17 16:34:31 +0000

    Added solution for exercise 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes int as paramter and updates value to 98
+ *
+ * @*n: The integer passed to function as parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
