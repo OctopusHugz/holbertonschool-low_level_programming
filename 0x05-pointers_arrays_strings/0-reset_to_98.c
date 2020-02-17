7c1e4d2df0753aeb8e245265087544a76cb40c70
Colson Scott <1670@holbertonschool.com>
2020-02-17 16:37:55 +0000

    Removed * from paramter description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes int as paramter and updates value to 98
  *
- * @*n: The integer passed to function as parameter
+ * @n: The integer passed to function as parameter
  */
 
 void reset_to_98(int *n)
