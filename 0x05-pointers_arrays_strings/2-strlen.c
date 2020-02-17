25a6bb204314998f2b102a07e967bda77f5a0878
Colson Scott <1670@holbertonschool.com>
2020-02-17 17:12:06 +0000

    Added solution for exercise 2 and updated holberton header file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: String to return the lenght of
+ *
+ * Return: 0 if success
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+	_putchar('\n');
+}
