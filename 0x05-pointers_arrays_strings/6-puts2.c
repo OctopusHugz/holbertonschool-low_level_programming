d1f35e5d0e838cc4bb3f3f3036a88d216b7f3b8d
Colson Scott <1670@holbertonschool.com>
2020-02-18 19:14:37 +0000

    Added solution for exercise 6 and updated holberton header file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to print every other character of
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (i = 0; i < len - 1; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
