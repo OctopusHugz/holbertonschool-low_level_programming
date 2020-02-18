48efeb8b23ff209ab423573afb18af359b6e3669
Colson Scott <1670@holbertonschool.com>
2020-02-18 21:11:28 +0000

    Added first attempt at exercise 9 and updated holberton header file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strcpy - copies the string pointed to by src to buffer pointed to by dest
+ * @dest: buffer destination to print to
+ * @src: string to copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = 0; i < len + 1; i++)
+	{
+		dest[i] = src[i];
+		printf("%c", src[i]);
+	}
+	return (dest);
+}
