32b0548a5e52f1d1ce8486e73112abdf3ab92031
Colson Scott <1670@holbertonschool.com>
2020-02-18 19:45:28 +0000

    Added solution for exercise 7 and updated holberton header file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string, followed by \n
+ * @str: string to print half of
+ */
+
+void puts_half(char *str)
+{
+	int i, j, len;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		i = len/2;
+	}
+	else
+	{
+		j = (len-1) / 2;
+		i = len - j;
+	}
+	for (; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
