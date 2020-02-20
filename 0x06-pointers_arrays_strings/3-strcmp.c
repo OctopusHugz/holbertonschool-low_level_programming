212376024f325812aa77f911504154066e2c7bfd
Colson Scott <1670@holbertonschool.com>
2020-02-20 20:45:34 +0000

    Added solution for exercise 3 and updated holberton header file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares first letter of two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: difference between ascii values of first letters
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j;
+	i = 0;
+	j = 0;
+
+	if (s1[i] == s2[j])
+	{
+		i++;
+		j++;
+	}
+
+	return (s1[0] - s2[0]);
+}
