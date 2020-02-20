dfec4b4914b9b67ae28d9a71e6c3bbba888e8aed
Colson Scott <1670@holbertonschool.com>
2020-02-20 23:16:29 +0000

    Added an attempt at exercise 2 with a while loop

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: buffer to append to
+ * @src: string to copy
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the string at dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	/* copy src into dest while src contains values and i < n */
+
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	/* return the copied string */
+	return (dest);
+}
