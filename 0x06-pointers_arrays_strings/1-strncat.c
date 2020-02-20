0654228cf5e6bdfce091a60d61ef42934e49a239
Colson Scott <1670@holbertonschool.com>
2020-02-20 20:23:53 +0000

    Added first solution for exercise 1 and updated holberton header file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strncat - concatenates two strings using at most n bytes
+ * @dest: string to copy to
+ * @src: string to copy
+ * @n: number of bytes to use
+ *
+ * Return: pointer to concatenated string at dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, len;
+
+	/* move to end of dest */
+
+	for (j = 0; dest[j];)
+	{
+		j++;
+	}
+
+	/* find len of src */
+	for (len = 0; src[len];)
+	{
+		len++;
+	}
+
+        /* loop through i and append to dest up until n bytes */
+
+	for (i = 0; i < n; i++, j++)
+	{
+		if (len < n && i == n - 1)
+		{
+			dest[j] = src[i];
+			dest[j] = '\0';
+		}
+		else
+		{
+			dest[j] = src[i];
+		}
+	}
+
+	/* if len < n, add \0
+	if (len < n)
+	{
+		dest[j] = '\0';
+		} */
+
+	/* return pointer to concatenated string */
+	return (dest);
+}
