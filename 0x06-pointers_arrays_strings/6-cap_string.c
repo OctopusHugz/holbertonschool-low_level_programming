d8c4d602e4a9dc3c5c4f3072505d4ef2f339fe28
Colson Scott <1670@holbertonschool.com>
2020-02-21 02:54:54 +0000

    Changed solution to print all capital letters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,32 +9,14 @@
 
 char *cap_string(char *str)
 {
-	int i, j;
-	char sep[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+	int i;
 
 	for (i = 0; str[i];)
 	{
-		if (i == 0)
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
-			else
-				i++;
-		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 		else
-		{
-			for (j = 0; str[i];)
-			{
-				if (sep[j] == str[i])
-				{
-					i++;
-					str[i] -= 32;
-				}
-				else
-					j++;
-			}
 			i++;
-		}
 	}
 	return (str);
 }
