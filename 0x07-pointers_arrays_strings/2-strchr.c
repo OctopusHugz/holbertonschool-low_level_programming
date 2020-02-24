3e237ea6b9a5a1c21936824d9a282833ae6e6a6f
Colson Scott <1670@holbertonschool.com>
2020-02-24 17:07:18 +0000

    Added solution for exercise 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search for char
+ * @c: char to locate
+ *
+ * Return: pointer to first occurrence of c in s or NULL if char is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		else if (s[i] == '\0')
+		{
+			break;
+		}
+	}
+	return (s);
+}
