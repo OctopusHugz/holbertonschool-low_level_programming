7e40c5d7d297ad20289d4d03ec58c995f5c0ffec
Colson Scott <1670@holbertonschool.com>
2020-02-24 21:33:58 +0000

    Added first solution for exercise 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#define NULL 0
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search
+ * @needle: string to find in haystack
+ *
+ * Return: pointer to beginning of located substring, or NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	j = 0;
+
+	for (i = 0; haystack[i];)
+	{
+		for (; needle[j];)
+		{
+			if (haystack[i] == needle[j])
+			{
+				j++;
+				return (haystack + i);
+				break;
+			}
+			else
+				i++;
+		}
+	}
+	return (NULL);
+}
