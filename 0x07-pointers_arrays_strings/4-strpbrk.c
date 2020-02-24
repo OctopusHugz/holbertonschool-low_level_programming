5d8baec8e27a05f0ba56213b6e859c770522d000
Colson Scott <1670@holbertonschool.com>
2020-02-24 21:14:08 +0000

    Added solution for exercise 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#define NULL 0
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string to search
@@ -10,5 +10,18 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j, len;
+
+	for (len = 0; accept[len]; len++)
+		;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
 	return (NULL);
 }
