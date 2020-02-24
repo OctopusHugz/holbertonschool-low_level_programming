c2116ca561f5413c674112a81e7f2aeed76fc74c
Colson Scott <1670@holbertonschool.com>
2020-02-24 21:03:15 +0000

    Added outline for exercise 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: set of bytes to search for in string
+ *
+ * Return: pointer to byte in s that matches one of the bytes in accept or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (s);
+}
