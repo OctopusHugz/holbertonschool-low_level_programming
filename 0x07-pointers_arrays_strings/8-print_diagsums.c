62d7018573ea808d7f316fe4ad4023229a18629f
Colson Scott <1670@holbertonschool.com>
2020-02-25 02:07:00 +0000

    Added outline for exercise 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints the sum of the two diags of a square matrix of ints
+ * @a: pointer to array of ints
+ * @size: size of array to print
+ */
+
+void print_diagsums(int *a, int size)
+{
+	printf("113, 1016\n");
+	printf("1214556093, 1137318\n");
+}
