b816614c953224cd5112bc8dc938457c0d83f84d
Colson Scott <1670@holbertonschool.com>
2020-02-25 02:17:15 +0000

    Added solution for exercise 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to string to set
+ * @to: value to set double pointer equal to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
