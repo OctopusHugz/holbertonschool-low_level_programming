1840434f087baf06e42588586e1f6db53a79ceb3
Colson Scott <1670@holbertonschool.com>
2020-02-26 22:50:13 +0000

    Added outline for exercise 7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - checks whether a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if string is a palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	if (s)
+		return (1);
+	else
+		return (0);
+}
