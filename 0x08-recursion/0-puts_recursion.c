6d7b9c10394fa19386ab43d5881bd8b9daefd378
Colson Scott <1670@holbertonschool.com>
2020-02-26 18:39:38 +0000

    Added putchar prototype to holberton header file and added solution to exercise 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - recursively prints a string followed by new line
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	i++;
+	_puts_recursion(s + i);
+}
