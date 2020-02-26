177aa7ffe24a2bcd2d4a65f901b38bed3ff96b74
Colson Scott <1670@holbertonschool.com>
2020-02-26 21:25:57 +0000

    Added solution for exercise 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * - _strlen_recursion - returns length of a string recursively
+ * @s: string to find the length of
+ *
+ * Return: length of string s
+ */
+
+int _strlen_recursion(char *s)
+{
+	static int len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		_strlen_recursion(s + 1);
+	}
+	return (len);
+}
