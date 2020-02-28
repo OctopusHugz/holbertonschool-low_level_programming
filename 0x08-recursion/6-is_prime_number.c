030c95a98612163cb0523f90587dc9cf13055a5a
Colson Scott <1670@holbertonschool.com>
2020-02-28 03:02:55 +0000

    Added solution for exercise 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,14 +3,34 @@
 /**
  * is_prime_number - checks if given integer is a prime number
  * @n: integer to check for primality
+ * @div: integer to use as divider and recurse
  *
  * Return: 1 if integer is prime number, otherwise return 0
  */
 
+int divider(int n, int div);
+
 int is_prime_number(int n)
 {
-	if (n == 1 || n % 2 == 0)
-		return (0);
-	else
+	return (divider(n, 3));
+}
+
+/**
+ * divider - divides n by div to check for primality
+ * @n: integer to check for primality
+ * @div: integer to use as divider and recurse
+ *
+ * Return: 1 if integer is prime, 0 otherwise
+ */
+
+int divider(int n, int div)
+{
+	if(n == div)
 		return (1);
+	else if(n % div == 0)
+		return (0);
+	else if (n % div != 0)
+		return (divider(n, div + 1));
+	else
+		return (-1);
 }
