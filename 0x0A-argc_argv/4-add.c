b6989d93e0895c6ed0595ef9879272f63036315d
Colson Scott <1670@holbertonschool.com>
2020-03-01 21:01:11 +0000

    Changed how program handles negative numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,41 +13,36 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
+	int i, sum, j;
 
 	sum = 0;
-	i = 1;
-	j = 0;
 
-	if (argc == 1)
-		printf("0\n");
-
-	else if (!isdigit(argv[i][j]))
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	else
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			j = 0;
-			while (argv[i][j])
+			for (j = 0; argv[i][j]; j++)
 			{
-				if (isdigit(argv[i][j]) && (atoi(argv[i]) >= 0))
+				if (argv[i][j] == '-')
 				{
-					sum += atoi(&argv[i][j]);
-					j++;
+					i++;
+					if (i == argc)
+						break;
 				}
-				else
+				else if (!isdigit(argv[i][j]))
 				{
 					printf("Error\n");
 					return (1);
 				}
 			}
+			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
 	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
