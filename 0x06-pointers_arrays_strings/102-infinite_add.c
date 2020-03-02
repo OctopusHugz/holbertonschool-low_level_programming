cf8f4141048920333d7a9bf2c94670e4b4335777
Colson Scott <1670@holbertonschool.com>
2020-03-02 22:04:40 +0000

    Fixed formatting issues

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -4,6 +4,7 @@
  * infinite_add - adds two numbers
  * @n1: first number to add
  * @n2: second number to add
+ * @r: buffer of result
  * @size_r: buffer size
  *
  * Return: pointer to result of addition
@@ -12,7 +13,7 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	if (*n1 && *n2 && size_r != 0)
-		return(r);
+		return (r);
 	else
 		return (r);
 }
