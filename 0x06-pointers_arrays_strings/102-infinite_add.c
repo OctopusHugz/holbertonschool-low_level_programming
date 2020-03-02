42246ec4b2310122fa42618ddeeb792d3592ebc5
Colson Scott <1670@holbertonschool.com>
2020-03-02 21:59:01 +0000

    Updated header file with infinite_add prototype and added function outline

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number to add
+ * @n2: second number to add
+ * @size_r: buffer size
+ *
+ * Return: pointer to result of addition
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return(r);
+}
