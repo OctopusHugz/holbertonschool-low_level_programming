353a96974d261dd374a003ee65b7e98ce780533b
Colson Scott <1670@holbertonschool.com>
2020-03-02 20:06:31 +0000

    Added outline for exercise 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include <holberton.h>
+
+/**
+ * atoi - translates ascii value to integer
+ * @s: string/char to turn into integer
+ *
+ * Return: integer value of s
+ */
+
+int _atoi(char *s)
+{
+	if (*s)
+		print_number(*(s + 1));
+	return(0);
+}
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
+}
