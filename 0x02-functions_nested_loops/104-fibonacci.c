42604dfdb165d728eb2447b5f1bbd291bfdca026
Colson Scott <1670@holbertonschool.com>
2020-03-02 18:39:50 +0000

    Added outline for exercise 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci numbers from 0 to 50
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	long int i, old_number, current_number, next_number;
+
+	/* Initialize numbers to start sequence */
+	old_number = 1;
+	current_number = 1;
+	i = 0;
+
+	while (i < 98)
+	{
+		if (i == 49)
+		{
+			printf("%ld\n", current_number);
+			break;
+		}
+		else
+		{
+			printf("%ld, ", current_number);
+			next_number = old_number + current_number;
+
+			old_number = current_number;
+			current_number = next_number;
+
+			i++;
+		}
+	}
+	return (0);
+}
