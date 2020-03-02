86fbf1b708180037f0932b62c38d81821dc7b176
Colson Scott <1670@holbertonschool.com>
2020-03-02 17:18:19 +0000

    Added solution for exercise 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all natural numbers that are multiples of 3 and 5 below 1024
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i, sum;
+
+	i = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
