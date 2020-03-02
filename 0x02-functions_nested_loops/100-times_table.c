667e3ed3cabb8a152cd1c137d501d39d2ec17a0f
Colson Scott <1670@holbertonschool.com>
2020-03-02 17:44:52 +0000

    Fixed most of output except when prod > 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_times_table - prints the nth times table
+ * @n: intenger to print times table of
+ */
+
+void print_times_table(int n)
+{
+	int row;
+	int col;
+	int prod;
+
+	if (n < 15 && n > 0)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				prod = row * col;
+				if (col != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (prod < 10)
+					{
+						_putchar(' ');
+					}
+					else
+					{
+					_putchar(prod / 10 + '0');
+					}
+					_putchar(prod % 10 + '0');
+				}
+				else
+				{
+					_putchar('0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
