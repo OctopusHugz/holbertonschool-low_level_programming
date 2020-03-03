cda27dc82f549899e048436232a07c97ad7d5a1b
Colson Scott <1670@holbertonschool.com>
2020-03-03 04:23:13 +0000

    Fixed solution for exercise 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,13 +9,24 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i, j, s1, s2;
 
+	s1 = 0;
+	s2 = 0;
+
+	/* find diagonal from top lef to bottom right */
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			printf("%d\n", a[i]);
-		}
+		s1 += *(a + (size * i + i));
 	}
+
+	/* find diagonal from bottom right to top left */
+	for (i = 0, j = size - 1; i < size; i++, j--)
+	{
+		if (i == 0)
+			s2 += *(a + (size * j));
+		else
+			s2 += *(a + (size * i - i));
+	}
+	printf("%d, %d\n", s1, s2);
 }
