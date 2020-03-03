b35faaf1693d38bbbbc3dc44090b90094eaa34fb
Colson Scott <1670@holbertonschool.com>
2020-03-03 18:07:11 +0000

    Added type cast for malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = malloc(sizeof(char) * size);
+	array = (char *) malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
