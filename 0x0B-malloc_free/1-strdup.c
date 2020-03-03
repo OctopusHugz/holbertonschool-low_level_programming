767cbfee092028360a855a76536675b19a209bb7
Colson Scott <1670@holbertonschool.com>
2020-03-03 17:41:54 +0000

    Added solution for exercise 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _strdup - copies a string into newly allocated memory and returns pointer
+ * @str: string to copy into new memory space
+ *
+ * Return: pointer to newly allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *array;
+
+	/* loop through str to find length and store in len if str != NULL*/
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len]; len++)
+		;
+
+	/* malloc memory for new array */
+
+	array = malloc(sizeof(char) * len);
+
+	/* copy str into new array */
+	for (i = 0; i < len; i++)
+		array[i] = str[i];
+	array[i] = '\0';
+
+	/* return pointer to newly allocated memory */
+	return (array);
+}
