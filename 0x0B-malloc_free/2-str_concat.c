01109354213e3c7f08710188e20fc18baf67a6ba
Colson Scott <1670@holbertonschool.com>
2020-03-03 22:22:35 +0000

    Added solution for exercise 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 #include "holberton.h"
 
 /**
@@ -8,75 +10,32 @@
  * Return: pointer to concatenated string
  */
 
-int _strlen(char *s);
-
-char *_strcat(char *dest, char *src);
-
 char *str_concat(char *s1, char *s2)
 {
+	int i, j, len1, len2;
 	char *array;
-	void *ptr;
+
+	len1 = 0;
+	len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	array = (char *) malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
-	array = _strcat(s1, s2);
-	ptr = &array;
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	array = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (array == NULL)
 		return (NULL);
 
-	free(array);
+	for (i = 0; i < len1; i++)
+		array[i] = s1[i];
 
-	return (ptr);
-}
+	for (j = 0; j < len2; i++, j++)
+		array[i] = s2[j];
 
-/**
- * _strcat - concatenates two strings
- * @dest: base string
- * @src: string to append
- *
- * Return: Pointer to string dest
- */
-
-char *_strcat(char *dest, char *src)
-{
-	int i, j, len;
-
-	/* get strlen and store in len */
-
-	for (len = 0; src[len]; len++)
-		;
-	/* move counter to null terminating byte */
-	for (i = 0; dest[i]; i++)
-		;
-	for (j = 0; j < len; i++, j++)
-	{
-		dest[i] = src[j];
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: String to return the lenght of
- *
- * Return: 0 if success
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-	int len = 0;
-
-	for (; s[i] != '\0'; i++)
-	{
-		len++;
-	}
-	return (len);
-	_putchar('\n');
+	return (array);
 }
