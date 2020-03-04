e352edc1b2a79067f95dd77001fef2688196d1aa
Colson Scott <1670@holbertonschool.com>
2020-03-04 05:21:31 +0000

    Added outline for exercise 100

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * strtow - splits a string into two words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings
+ */
+
+char **strtow(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+
+	return (0);
+}
