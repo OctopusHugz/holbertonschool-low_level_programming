1a50ae5894829f0b0c68550dcd70ba51c333cd2b
Colson Scott <1670@holbertonschool.com>
2020-03-05 17:56:17 +0000

    Fixed formatting issues

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,15 @@
 /**
  * malloc_checked - allocates b number of bytes in memory using malloc
  * @b: number of bytes to malloc
+ * Return: none
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *vp;
+
 	vp = malloc(b);
+
 	if (vp == NULL)
 		exit(98);
 	return (vp);
