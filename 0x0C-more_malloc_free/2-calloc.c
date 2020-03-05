d936975c70668a477357bc2991afb21dbe8e7e1e
Colson Scott <1670@holbertonschool.com>
2020-03-05 18:47:53 +0000

    Tried allocating memory for null byte

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,13 +15,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(nmemb * size);
+	array = malloc(nmemb * size + 1);
 
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
-		array[i] = '\0';
+		array[i] = 0;
 
 	return (array);
 }
