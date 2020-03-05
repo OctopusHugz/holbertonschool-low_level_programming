b839f347ef9c7edf156c1598e514ff8eec091f49
Colson Scott <1670@holbertonschool.com>
2020-03-05 19:40:38 +0000

    Changed logic to exclude negative numbers

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *array;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
 	array = (char *) malloc(nmemb * size);
