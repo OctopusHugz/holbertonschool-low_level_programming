9b8247806c44a1c77feca81a7675c947651976f7
Colson Scott <1670@holbertonschool.com>
2020-03-05 17:18:52 +0000

    Added solution for exercise 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - allocates b number of bytes in memory using malloc
+ * @b: number of bytes to malloc
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *vp;
+	vp = malloc(b);
+	if (vp == NULL)
+		exit(98);
+	return (vp);
+}
