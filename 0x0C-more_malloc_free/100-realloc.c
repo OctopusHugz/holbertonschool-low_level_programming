2848ad2d328d58d427fe98a3c3d0febc2235f7dd
Colson Scott <1670@holbertonschool.com>
2020-03-06 20:46:39 +0000

    Added solution for exercise 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,62 +1,51 @@
 #include "holberton.h"
-
-/**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to memory previously allocated by malloc
- * @old_size: size, in bytes, of allocated space for ptr
- * @new_size: new size, in bytes, of memory block
- *
- * Return: void pointer to reallocated memory block
- */
+#include <stdlib.h>
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int true_size;
+	unsigned int u;
+	char *mem;
+	char *copy;
+	copy = ptr;
 
-	if (old_size == new_size)
+	if (new_size == old_size)
+	{
 		return (ptr);
-
+	}
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 		if (ptr == NULL)
 		{
-			free(ptr);
 			return (NULL);
 		}
+		return(ptr);
 	}
-
-	/* find size_diff to determine # of mem blocks to leave uninitialized 
-	if (new_size > old_size)
-	size_diff = new_size - old_size; */
-
-	if (new_size > old_size)
-		true_size = old_size;
-	else if (new_size < old_size)
-		true_size = new_size;
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-}
-
-/**
- * _memset - fills memory with a constant byte
- * @s: memory area to fill
- * @b: constant byte to fill memory area with
- * @n: number of bytes to fill
- *
- * Return: pointer to memory area s
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
+	mem = malloc(new_size);
+	if (mem == NULL)
+	{
+		free(mem);
+		return (NULL);
+	}
+	if (new_size < old_size)
+	{
+		for (u = 0; u < new_size; u++)
+		{
+			mem[u] = copy [u];
+		}
+	}
+	if (new_size > old_size)
+	{
+		for (u = 0; u < old_size; u++)
+		{
+			mem[u] = copy[u];
+		}
+	}
+	free(ptr);
+	return (mem);
 }
