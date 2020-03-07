e6014455338e26f84b258bce4d0c7703d91b1286
Colson Scott <1670@holbertonschool.com>
2020-03-07 00:11:52 +0000

    Added updated solution for exercise 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -4,16 +4,37 @@
  * argstostr - concatenates all the arguments of the program
  * @ac: argc or number of arguments provided to program
  * @av: argv or array storing arguments
- *
  * Return: pointer to new string or NULL if it fails
  */
 
 char *argstostr(int ac, char **av)
 {
-	char *array = NULL;
+	char *array;
+	int rows, cols, av_len, i;
+
+	rows = 0, av_len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
+	/* find strlen of av[rows][cols] and store in av_len */
+	for (cols = 0; av[rows][cols]; rows++)
+		for (; av[rows][cols]; cols++)
+			av_len++;
+
+	/* malloc memory for new array and null check */
+	array = malloc(av_len * sizeof(char) + 1);
+	if (array == NULL)
+		return (NULL);
+
+	/* start copying av into array */
+	rows = 0, i = 0;
+	for (cols = 0; av[rows][cols]; rows++)
+	{
+		for (; av[rows][cols]; i++, cols++)
+			array[i] = av[rows][cols];
+		array[i] = '\n';
+	}
+	array[i + 1] = '\0';
 	return (array);
 }
