a7557ef8a8a10ec59c025128e5ac1454cb5aeec7
Colson Scott <1670@holbertonschool.com>
2020-03-09 17:49:02 +0000

    Added return if d == NULL)

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#define NULL 0
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -10,6 +11,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
