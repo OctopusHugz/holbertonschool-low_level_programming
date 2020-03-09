0fbab8cd279fd1b7575370d50513fc0b460f55b1
Colson Scott <1670@holbertonschool.com>
2020-03-09 17:05:47 +0000

    Fixed formatting issues and added struct dog description and parameter descriptions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defines a structure called dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 struct dog
 {
-	char *name;				\
-	float age;				\
-	char *owner;				\
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
