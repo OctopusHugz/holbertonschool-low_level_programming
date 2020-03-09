5e3503b7696e318791ec13e4c5c5bed8a4c20133
Colson Scott <1670@holbertonschool.com>
2020-03-09 02:52:00 +0000

    Fixed solution for rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,5 +9,18 @@
 
 char *rot13(char *str)
 {
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'M')
+			str[i] += 13;
+		else if (str[i] >= 'a' && str[i] <= 'm')
+			str[i] += 13;
+		else if (str[i] >= 'N' && str[i] <= 'z')
+			str[i] -= 13;
+		else if (str[i] >= 'n' && str[i] <= 'z')
+			str[i] -= 13;
+	}
 	return (str);
 }
