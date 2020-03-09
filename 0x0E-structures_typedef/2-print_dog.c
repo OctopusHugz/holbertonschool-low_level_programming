9baa543f7db286abba645d6f53e5f0c700ef9f11
Colson Scott <1670@holbertonschool.com>
2020-03-09 19:00:35 +0000

    Added printf with ''\n' if d == NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,15 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		return;
+		printf("\n");
 	if (d->name == NULL)
-		printf("Name: (nil) \n");
+		printf("Name: (nil)\n");
 	else
-		printf("Name: %s\n", (*d).name);
+		printf("Name: %s\n", d->name);
 	if (d->age == 0.0)
 		printf("(nil)\n");
 	else
-		printf("Age: %f\n", (*d).age);
+		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("(nil)\n");
 	else
