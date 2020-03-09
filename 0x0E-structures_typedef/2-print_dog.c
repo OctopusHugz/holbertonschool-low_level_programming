4245c39a707b16d90859254f82efc20d0a940440
Colson Scott <1670@holbertonschool.com>
2020-03-09 19:04:59 +0000

    Fixed logic if age == 0

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,12 +9,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("\n");
+		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == 0.0)
+	if (d->age == 0)
 		printf("(nil)\n");
 	else
 		printf("Age: %f\n", d->age);
