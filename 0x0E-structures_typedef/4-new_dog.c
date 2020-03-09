1889ea2e9024e36389924ae355c4b431a4d1a676
Colson Scott <1670@holbertonschool.com>
2020-03-09 20:07:50 +0000

    Added _strdup to store a copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,12 +12,53 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *dup_name, *dup_owner;
+
+	dup_name = _strdup(name);
+	dup_owner = _strdup(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	dog->name = dup_name;
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = dup_owner;
 	return (dog);
+}
+
+/**
+ * _strdup - copies a string into newly allocated memory and returns pointer
+ * @str: string to copy into new memory space
+ *
+ * Return: pointer to newly allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *array;
+
+	/* loop through str to find length and store in len if str != NULL */
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len]; len++)
+		;
+
+	/* malloc memory for new array and return NULL if not enough memory */
+
+	array = (char *) malloc(sizeof(char) * len + 1);
+
+	if (array == NULL)
+		return (NULL);
+
+	/* copy str into new array */
+
+	for (i = 0; i < len; i++)
+		array[i] = str[i];
+
+	/* return pointer to newly allocated memory */
+
+	return (array);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,6 +25,8 @@ void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
 
+char *_strdup(char *str);
+
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
