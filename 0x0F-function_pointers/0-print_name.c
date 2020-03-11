b2819e407a76b93450b7a02197d672d0db70080f
Colson Scott <1670@holbertonschool.com>
2020-03-11 13:32:25 +0000

    Added solution for exercise 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is or in uppercase
+ * @name: name to print
+ * @f: pointer to function to call
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
