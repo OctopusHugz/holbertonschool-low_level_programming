b88fee5011bcc70f515392e164630d7f77d2b48b
Colson Scott <1670@holbertonschool.com>
2020-03-11 17:07:19 +0000

    Added null check for array and fixed indents

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,13 +13,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if ((cmp(array[i]) != 0))
-		    return (i);
+			return (i);
 	}
 	if (i == size - 1)
 		return (-1);
