5d6b10517cdf74356e0491c3596c299041b6c496
Colson Scott <1670@holbertonschool.com>
2020-03-12 03:24:55 +0000

    Fixed formatting issues

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints opcodes of its own main function
+ * @argc: number of arguments passed to program
+ * @argv: array storing arguments as strings
+ * Return: 0 if success
+ */
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
