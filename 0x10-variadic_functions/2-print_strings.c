14cc1be61fbf0b3e3a6c6b67b2a84b3f62db63e8
Colson Scott <1670@holbertonschool.com>
2020-03-13 16:13:18 +0000

    Added first outline for exercise 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between the strings
+ * @n: number of strings to print
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arguments;
+	unsigned int i;
+
+	va_start(arguments, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL || i == n - 1)
+			printf("%s", va_arg(arguments, char *));
+		else if (separator != NULL && va_arg(arguments, char *) != NULL)
+			printf("%s%s", va_arg(arguments, char *), separator);
+		else if (va_arg(arguments, char *) == NULL)
+		  printf("(nil)%s", separator);
+	}
+	putchar('\n');
+	va_end(arguments);
+}
