aedb65b34187630b1faaf282a48faf0351be9b0f
OctopusHugz <colsonscott53@gmail.com>
2020-03-19 19:49:04 +0000

    Added check if head == NULL and also added free(new_node)

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = *head;
 
+	if (head == NULL)
+		return (NULL);
+
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -20,4 +23,5 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->next = *head;
 	*head = new_node;
 	return (*head);
+	free(new_node);
 }
