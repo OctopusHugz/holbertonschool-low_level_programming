61458711cdd395cbec407641373310a403bb28ff
OctopusHugz <colsonscott53@gmail.com>
2020-03-19 18:33:33 +0000

    Added solution for exercise 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,5 +8,18 @@
 
 size_t list_len(const list_t *h)
 {
+	int count = 0;
+	const list_t *current_node = h;
 
+	if (h == NULL)
+		return (0);
+
+	while (current_node != NULL)
+	{
+		count++;
+		current_node = current_node->next;
+		if (current_node == NULL)
+			return (count);
+	}
+	return (count);
 }
