15ed9e0defc57de95e9761ca3c524b97ea6af49e
OctopusHugz <colsonscott53@gmail.com>
2020-03-19 20:25:03 +0000

    Added solution for exercise 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,5 +9,42 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *tail = *head;
 
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+
+	while (tail->next != NULL)
+		tail = tail->next;
+	tail->next = new_node;
+	return (tail);
+}
+
+
+/**
+ * _strlen - returns the length of a string
+ * @string: string to return the lenght of
+ *
+ * Return: 0 if success
+ */
+
+int _strlen(const char *string)
+{
+	int len;
+
+	for (len = 0; string[len]; len++)
+		;
+
+	return (len);
 }
