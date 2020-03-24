e5d69530551d0add9f476b98c499673c4d26e92b
OctopusHugz <colsonscott53@gmail.com>
2020-03-24 18:42:22 +0000

    Added outline for exercise 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t linked list
+ * @h: pointer to pointer of head of the list
+ *
+ * Return: size of the list that was freed
+ **/
+
+size_t free_listint_safe(listint_t **h)
+{
+	if (h == NULL)
+		return (0);
+
+	return (0);
+}
