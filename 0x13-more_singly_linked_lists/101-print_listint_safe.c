91da6cf5703ac4bcb8fee31305b5a2dbf8c59ccf
OctopusHugz <colsonscott53@gmail.com>
2020-03-24 20:33:05 +0000

    Added solution for exercise 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "lists.h"
 
 /**
@@ -9,7 +10,28 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *current = head;
+	const listint_t *insert = current;
+	int count = 0;
+
 	if (head == NULL)
 		exit(98);
+
+	while (current)
+	{
+		count++;
+		printf("current is: [%lu] %4d,  ", (size_t)current, current->n);
+		printf("current->next is: [%lu],  ", (size_t)current->next);
+		printf("insert is: [%lu]\n", (size_t)insert);
+		if ((size_t)current != (size_t)insert - 32 &&
+		    current->next && count != 1)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			exit(98);
+		}
+		insert = current;
+		current = current->next;
+		sleep(1);
+	}
 	return (0);
 }
