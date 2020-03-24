43966908ebcf49b12f8a18355d5b0d12bc000bc9
OctopusHugz <colsonscott53@gmail.com>
2020-03-24 17:15:33 +0000

    Added free statement for new_node if element is unable to be added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	if (current_node == NULL && idx != 0)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
 	new_node->n = n;
 	new_node->next = current_node->next;
