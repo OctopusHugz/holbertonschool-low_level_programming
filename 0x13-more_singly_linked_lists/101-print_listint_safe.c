7e9566bf5e1197a622f4826ead895079dbfe4e25
OctopusHugz <colsonscott53@gmail.com>
2020-03-24 18:36:39 +0000

    Added outline for exercise 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to head of the linked list
+ *
+ * Return: number of nodes in the list
+ **/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	return (0);
+}
