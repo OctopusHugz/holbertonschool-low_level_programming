c9797e51a63b01b22863068c570dbf09209fe923
OctopusHugz <colsonscott53@gmail.com>
2020-03-25 06:15:25 +0000

    Added test files to tests dir

---
+++ 0x13-more_singly_linked_lists/tests/101-main.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *head2;
+	listint_t *node;
+	int count = 0;
+	
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
+	add_nodeint(&head2, 98);
+	add_nodeint(&head2, 402);
+	add_nodeint(&head2, 1024);
+	count = print_listint_safe(head2);
+	printf("Count is %d\n", count);
+	head = NULL;
+	node = add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	node->next = add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	
+
+	count = print_listint_safe(head);
+	printf("Count is %d\n", count);
+	return (0);
+}
