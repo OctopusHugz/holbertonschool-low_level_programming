7d43763931a4799641ce9e7327fa03bf25fcc262
OctopusHugz <colsonscott53@gmail.com>
2020-03-25 21:42:45 +0000

    Added loop_finder function and incorporated

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -16,10 +16,37 @@ listint_t *find_listint_loop(listint_t *head)
 
 	while (node)
 	{
-		if (node->next >= node)
-			return (node);
-		node = node->next;
+		node = loop_finder(head);
+		return (node);
 	}
 	node = NULL;
 	return (node);
+}
+
+listint_t *loop_finder(listint_t *head)
+{
+	listint_t *tort;
+	listint_t *hare;
+
+	if (head == NULL)
+		return (NULL);
+
+	tort = head;
+	hare = head;
+	while (hare->next && hare->next->next)
+	{
+		tort = tort->next;
+		hare = hare->next->next;
+		if (tort == hare)
+		{
+			tort = head;
+			while (tort != hare)
+			{
+				tort = tort->next;
+				hare = hare->next;
+			}
+			return (hare);
+		}
+	}
+	return (NULL);
 }
