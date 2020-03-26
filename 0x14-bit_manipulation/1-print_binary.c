7b59965254f7725e0ce271ac146c036236f58e53
OctopusHugz <colsonscott53@gmail.com>
2020-03-26 23:51:23 +0000

    Removed % and / from function, added is_even function and updated header file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -17,14 +17,34 @@ void print_binary(unsigned long int n)
 	{
 		_putchar('1');
 	}
-	else if (n % 2 == 0)
+	else if (is_even(n))
 	{
-		print_binary(n / 2);
+		print_binary(n >> 1);
 		_putchar('0');
 	}
-	else if (n % 2 == 1)
+	else
 	{
-		print_binary(n / 2);
+		print_binary(n >> 1);
 		_putchar('1');
 	}
+}
+
+/**
+ * is_even - checks if number is odd or even
+ * @n: decimal number to find if even
+ *
+ * Return: 1 if even, 0 if odd
+ */
+
+int is_even(unsigned long int n)
+{
+	unsigned long int i = 2;
+
+	while (i <= n)
+	{
+		i += 2;
+		if (i == n)
+			return (1);
+	}
+	return (0);
 }
