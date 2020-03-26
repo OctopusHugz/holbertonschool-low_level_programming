254191187bd05b9d863364cce6669dd64fa7affe
OctopusHugz <colsonscott53@gmail.com>
2020-03-26 23:06:33 +0000

    Added solution for exercise 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <string.h>
+#include <limits.h>
+
+/**
+ * binary_to_uint - converts a binary numnber to an unsigned int
+ * @b: binary number to convert
+ *
+ * Return: decimal value of binary number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int len;
+	unsigned int i = 0, place_mult = 1, num = 0;
+	char *string = malloc(strlen(b));
+
+	if (string == NULL || b == NULL)
+		return (0);
+
+	for (len = 0; b[len];)
+	{
+		if (b[len] == '0' || b[len] == '1')
+			len++;
+		else
+			return (0);
+	}
+	len--;
+
+	while (len >= 0)
+	{
+		string[i] = b[len];
+		len--;
+		i++;
+	}
+
+	for (i = 0; string[i]; i++)
+	{
+		if (string[i] == '1')
+			num += (1 * place_mult);
+		place_mult *= 2;
+	}
+
+	return (num);
+}
