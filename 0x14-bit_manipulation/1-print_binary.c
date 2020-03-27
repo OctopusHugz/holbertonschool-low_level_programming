12d96f92602046a054b8c3dab2bb005a26dfb252
OctopusHugz <colsonscott53@gmail.com>
2020-03-27 15:00:07 +0000

    Removed is_even function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,23 +12,3 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 	_putchar((n & 1) + '0');
 }
-
-/**
- * is_even - checks if number is odd or even
- * @n: decimal number to find if even
- *
- * Return: 1 if even, 0 if odd
- */
-
-int is_even(unsigned long int n)
-{
-	unsigned long int i = 0;
-
-	while (i <= n)
-	{
-		i += 2;
-		if (i == n)
-			return (1);
-	}
-	return (0);
-}
