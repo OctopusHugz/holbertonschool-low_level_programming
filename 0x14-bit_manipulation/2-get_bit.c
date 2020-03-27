7223a905b5bf0a94ed3cce0adf70fb5994574621
OctopusHugz <colsonscott53@gmail.com>
2020-03-27 03:09:05 +0000

    Add solution for exercise 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * get_bit - gets the bit at a given index
+ * @n: number to find the bit value of
+ * @index: bit index to find the value of
+ *
+ * Return: the bit's value at the given index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	if ((n >> (index)) & 1)
+		return (1);
+	else
+		return (0);
+}
