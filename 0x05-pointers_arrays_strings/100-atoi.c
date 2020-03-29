ae5a31ea248a07a5a2da434676d34bbd90dd41bf
OctopusHugz <colsonscott53@gmail.com>
2020-03-29 19:57:49 +0000

    Added solution for exercise 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,21 @@
 
 int _atoi(char *s)
 {
-	if (*s)
-		s++;
-	return (0);
+	int num = 0, i = 0, negs = 0;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+			negs++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+		i++;
+	}
+	if (negs % 2 != 0)
+		num = -num;
+	return (num);
 }
