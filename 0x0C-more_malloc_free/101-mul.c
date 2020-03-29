09ba96d3dd9caf8a9110c940e566d9b529d50191
OctopusHugz <colsonscott53@gmail.com>
2020-03-29 17:52:06 +0000

    Added outline for exercise 101

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - multiplies two numbers provided from command line
+ * @argc: number of arguments provided
+ * @argv: array of strings containing arguments
+ *
+ * Return: 0 if success, 98 if error
+ **/
+
+int main(int argc, char **argv)
+{
+}
