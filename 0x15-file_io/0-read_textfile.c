67eb67f04e70243e432827b911fa06612914738e
OctopusHugz <colsonscott53@gmail.com>
2020-03-31 19:27:21 +0000

    Added includes to header file and added solution for exercise 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * read_textfile -
+ *
+ * Return:
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, count = 0;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	count = read(fd, buf, letters);
+	if (count == -1)
+		return (0);
+	buf[letters - 1] = '\0';
+	if ((size_t)count != letters)
+		count++;
+
+	if ((size_t)write(STDOUT_FILENO, buf, letters) != letters)
+		return (0);
+
+	return (count);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 
