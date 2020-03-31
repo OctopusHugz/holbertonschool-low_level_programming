51afc3d57721dc5f154fac301ca70a170bc7bad8
OctopusHugz <colsonscott53@gmail.com>
2020-03-31 20:44:52 +0000

    Add string.h to header file and added solution for exercise 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,34 @@
 #include "holberton.h"
 
 /**
- * create_file -
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: null terminated string of text content to write to file
  *
- * Return: 
- */
+ * Return: 1 on success or -1 on failure
+ **/
+
 int create_file(const char *filename, char *text_content)
 {
+	int fd, wr, len;
 
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	len = strlen(text_content);
+
+	wr = write(fd, text_content, len);
+	if (wr == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
 }
