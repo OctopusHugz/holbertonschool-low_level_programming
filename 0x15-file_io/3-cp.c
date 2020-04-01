51566bbb45e0b93f173313b6990e28e3c63c4b78
OctopusHugz <colsonscott53@gmail.com>
2020-04-01 03:02:46 +0000

    Add solution for exercise 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * main - copies the contents of a file to another file
+ * @argc: number of arguments provided at command line
+ * @argv: pointer to an array of strings containing CL args
+ *
+ * Return: 0 if success, -1 otherwise
+ **/
+
+int main(int argc, char *argv[])
+{
+	int fd, fd_two, rd = 10, wr;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	fd_two = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd_two == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while (rd > 0)
+	{
+		rd = read(fd, buf, 1024);
+		if (rd == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		if (rd > 0)
+		{
+			wr = write(fd_two, buf, rd);
+			if (wr == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
+	}
+	close(fd), close(fd_two);
+	if ((!close(fd)) || !(close(fd_two)))
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	return (0);
+}
