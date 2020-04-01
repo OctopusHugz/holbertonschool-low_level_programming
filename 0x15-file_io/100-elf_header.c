dfdbc5b535f4ddcb5459a8b3d3513e10fc943874
OctopusHugz <colsonscott53@gmail.com>
2020-04-01 03:27:50 +0000

    Added outline for exercise 100

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - displays the information contained in
+ * the ELF header at the start of an ELF file
+ * @argc: number of arguments provided at command line
+ * @argv: pointer to an array of strings containing CL args
+ *
+ * Return: 0 if success, -1 otherwise
+ **/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		return (-1);
+
+	return (0);
+}
