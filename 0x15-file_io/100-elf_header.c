3c959d4df7a1a6f5d6f31d0ad4c447d28457e31b
OctopusHugz <colsonscott53@gmail.com>
2020-04-01 06:10:30 +0000

    Added updated outline

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fd, rd = 10, wr;
+	int fd, rd = 10, wr, class, data;
 	char buf[1024];
 
 	if (argc != 2)
@@ -26,7 +26,18 @@ int main(int argc, char *argv[])
 		if (rd == -1)
 			return (-1);
 		printf("ELF Header:\n");
-		printf("  Magic: %x %x %x %x\n", buf[EI_MAG0], buf[EI_MAG1], buf[EI_MAG2], buf[EI_MAG3]);
+
+		printf("  Magic: %x %x %x %x %.2x %.2x\n", buf[EI_MAG0], buf[EI_MAG1], buf[EI_MAG2], buf[EI_MAG3], buf[4], buf[5]);
+
+		class = buf[EI_CLASS];
+		if (class == 2)
+			printf("  Class: %32s", "ELF64\n");
+
+		data = buf[EI_DATA];
+		if (data == 1)
+			printf("  Data: %57s", "2's complement, little endian\n");
+		printf("  Version: %25d (current)\n", buf[EI_VERSION]);
+		printf("  OS/ABI: %d\n", buf[EI_OSABI]);
 		if (rd > 0)
 		{
 			wr = write(fd, buf, rd);
