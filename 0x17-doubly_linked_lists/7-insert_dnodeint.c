509c23737c52e1084e45c74fb816f42983fc0437
OctopusHugz <colsonscott53@gmail.com>
2020-05-02 18:31:27 +0000

    Reverted back to old logic missing 1 check

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,13 +15,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *node, *temp;
 	unsigned int count = 0;
 
-	if (h == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
+	if (*h == NULL)
+	{
+		new_node->next = NULL;
+		*h = new_node;
+		return (*h);
+	}
 	node = *h;
 	while (node && count <= idx - 1)
 	{
