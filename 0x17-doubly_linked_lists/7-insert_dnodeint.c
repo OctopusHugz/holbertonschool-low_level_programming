79ebaa29651ea808aa77d39af0fe0eec6794fe22
OctopusHugz <colsonscott53@gmail.com>
2020-05-02 18:34:53 +0000

    Added logic to only create list from empty list if idx == 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
-	if (*h == NULL)
+	if (*h == NULL && idx == 0)
 	{
 		new_node->next = NULL;
 		*h = new_node;
