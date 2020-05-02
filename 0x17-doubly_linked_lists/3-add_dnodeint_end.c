40ebc9ae25acbaa7ed1cacee1ce82c1da9ae0e65
OctopusHugz <colsonscott53@gmail.com>
2020-05-02 18:14:35 +0000

    Assigned tail to new_node->prev instead of *head

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -27,7 +27,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (tail->next)
 		tail = tail->next;
 	tail->next = new_node;
-	/* new_node->prev = *head; */
+	new_node->prev = tail;
 	tail = new_node;
 	return (tail);
 }
