f087b54b11c76aa1bb05aafc45e86289176f88d5
OctopusHugz <colsonscott53@gmail.com>
2020-05-02 18:25:41 +0000

    Added logic if index == 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,9 +19,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->prev = NULL;
 	if (*h == NULL)
 	{
-		new_node->prev = NULL;
 		new_node->next = NULL;
 		*h = new_node;
 		return (*h);
@@ -29,6 +29,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	node = *h;
 	while (node && count <= idx - 1)
 	{
+		if (idx == 0)
+		{
+			new_node->next = node;
+			node->prev = new_node;
+			*h = new_node;
+			return (*h);
+		}
 		if (count == idx - 1 && node->next)
 		{
 			temp = node->next;
