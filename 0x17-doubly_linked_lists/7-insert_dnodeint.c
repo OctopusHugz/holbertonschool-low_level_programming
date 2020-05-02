c24bd0f3185405d3ebd0afdf67e3dff9cd1d763d
OctopusHugz <colsonscott53@gmail.com>
2020-05-02 19:03:19 +0000

    Fixed logic to handle insert last node in the list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,21 +12,17 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *node, *temp;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *node = *h, *temp;
 	unsigned int count = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
-	new_node->prev = NULL;
+	new_node->n = n, new_node->prev = NULL, new_node->next = NULL;
 	if (*h == NULL && idx == 0)
 	{
-		new_node->next = NULL;
 		*h = new_node;
 		return (*h);
 	}
-	node = *h;
 	while (node && count <= idx - 1)
 	{
 		if (idx == 0)
@@ -36,18 +32,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			*h = new_node;
 			return (*h);
 		}
-		if (count == idx - 1 && node->next)
+		if (count == idx - 1)
 		{
-			temp = node->next;
-			node->next = new_node;
-			new_node->prev = node;
-			new_node->next = temp;
-			temp->prev = new_node;
+			if (node->next)
+			{
+				temp = node->next;
+				node->next = new_node;
+				new_node->prev = node;
+				new_node->next = temp;
+				temp->prev = new_node;
+			}
+			else
+				new_node->prev = node, node->next = new_node;
 			return (new_node);
 		}
 		count++;
 		node = node->next;
 	}
-	free(new_node);
 	return (NULL);
 }
