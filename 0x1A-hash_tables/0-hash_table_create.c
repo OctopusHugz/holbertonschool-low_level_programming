c432f27bf9e2c0b32da9d2441c6bfeb1c8e2386b
OctopusHugz <colsonscott53@gmail.com>
2020-05-28 15:41:52 +0000

    Added first attempt at exercise 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array of the hash table
+ *
+ * Return: pointer to newly created hash table, or NULL if it fails
+ **/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht = malloc(sizeof(hash_table_t) * size);
+
+	if (new_ht == NULL)
+		return (NULL);
+	return (new_ht);
+}
