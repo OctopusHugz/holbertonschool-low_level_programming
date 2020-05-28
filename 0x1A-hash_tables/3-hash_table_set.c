c9a63b6be5e75bf5cbfb72ec8ed6e167d208febc
OctopusHugz <colsonscott53@gmail.com>
2020-05-28 20:16:54 +0000

    Removed extraneous code from 3-hash_table_set.c and added updated header file with add_node prototype

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,67 +9,6 @@
  * Return: 1 if suceeded, 0 otherwise
  **/
 
-/* int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	hash_node_t *new_node;
-	unsigned long int index = 0;
-
-	if (strcmp(key, "") == 0)
-		return (0);
-	new_node = malloc(sizeof(hash_node_t) * 1);
-	if (new_node == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
-} */
-
-/* int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	hash_node_t *new_node;
-	unsigned long int index = 0;
-
-	if (strcmp(key, "") == 0)
-		return (0);
-	new_node = malloc(sizeof(hash_node_t) * 1);
-	if (new_node == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
-	else
-	{
-		new_node->next = ht->array[index];
-		ht->array[index] = new_node;
-	}
-	return (1);
-} */
-
-/* int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	hash_node_t *new_node, *temp;
-	unsigned long int index = 0;
-
-	if (strcmp(key, "") == 0)
-		return (0);
-	new_node = malloc(sizeof(hash_node_t) * 1);
-	if (new_node == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	temp = ht->array[index];
-	new_node->next = temp;
-	ht->array[index] = new_node;
-	return (1);
-} */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
@@ -80,6 +19,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = add_node(ht->array[index], key, value);
 	return (1);
 }
+
+/**
+ * add_node - adds a node to a hash table index
+ * @head: head node of linked list at hash table index
+ * @key: key to add to hash table
+ * @value: value associated with key to add to hash table
+ *
+ * Return: address of new node if successful, return NULL if failure
+ **/
 
 hash_node_t *add_node(hash_node_t *head, const char *key, const char *value)
 {
