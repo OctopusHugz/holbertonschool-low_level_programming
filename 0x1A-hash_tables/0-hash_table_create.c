1c8373f9dbb1049f85e38a0738fd2533864198f3
OctopusHugz <colsonscott53@gmail.com>
2020-05-28 21:53:01 +0000

    Init new hash table to all NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht = malloc(sizeof(hash_table_t) * 1);
+	unsigned long int index = 0;
 
 	if (new_ht == NULL)
 		return (NULL);
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_ht->array == NULL)
 		return (NULL);
+	while (index < size)
+	{
+		new_ht->array[index] = NULL;
+		index++;
+	}
 	new_ht->size = size;
 	return (new_ht);
 }
