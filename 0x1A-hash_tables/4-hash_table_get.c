140f13f797e8aee5dbb4111d2b354f74b04d013a
OctopusHugz <colsonscott53@gmail.com>
2020-05-29 17:28:36 +0000

    Moved index variable assignment below ht == NULL check

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,11 +10,12 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 	hash_node_t *current;
 
-	if (ht->array == NULL)
+	if (ht == NULL)
 		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index])
 	{
 		current = ht->array[index];
