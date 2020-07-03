fd9baae9c16b0101afaa22f2122c513c03e0facc
OctopusHugz <colsonscott53@gmail.com>
2020-07-03 01:26:28 +0000

    Added outline for exercise 5

---
+++ 0x1C-makefiles/5-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module creates the island_perimeter function"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    pass
