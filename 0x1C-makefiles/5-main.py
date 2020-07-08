b4bbe10cc8b87ea01f0e1d4a988d77d353da1a9d
OctopusHugz <colsonscott53@gmail.com>
2020-07-08 04:27:46 +0000

    Removed 5-main.pyp

--- 0x1C-makefiles/5-main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""This module creates the island_perimeter function"""
-
-
-def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    pass
