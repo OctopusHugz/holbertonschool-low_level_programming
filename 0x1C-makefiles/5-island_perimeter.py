eb26395817092c1ecfa05ba6012b01ffbea3b8a3
OctopusHugz <colsonscott53@gmail.com>
2020-07-08 04:25:00 +0000

    Added simpler solution

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,24 +5,15 @@
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
     land_coords = []
-    height = []
-    width = []
     sides = 0
     x = 0
-    if (len(grid) + len(grid[0])) < 100:
-        for rows in grid:
-            y = 0
-            for nums in rows:
-                if nums == 1:
-                    sides += 4
-
-                    land_coords.append((x, y))
-                y += 1
-            x += 1
-        for tups in land_coords:
-            height.append(tups[0])
-            width.append(tups[1])
-        height = (max(height) - min(height))
-        width = (max(width) - min(width))
-        shared_sides = height * 2 + width * 2
-        return sides - shared_sides
+    for rows in grid:
+        y = 0
+        for nums in rows:
+            if nums == 1:
+                sides += 4
+                land_coords.append((x, y))
+            y += 1
+        x += 1
+    sides -= (len(land_coords) - 1) * 2
+    return sides
