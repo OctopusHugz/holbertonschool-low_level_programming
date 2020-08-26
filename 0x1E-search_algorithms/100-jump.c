c33367a40e67a76d40f24192ba1c395dc9bf725e
OctopusHugz <colsonscott53@gmail.com>
2020-08-26 18:10:04 +0000

    Revert back to start < size from start <= size

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 
 	if (!array)
 		return (-1);
-	for (; start <= size; start += jump)
+	for (; start < size; start += jump)
 	{
 		printf("Value checked array[%d] = [%d]\n", start, array[start]);
 		if (array[start + jump] >= value || start + jump > size)
