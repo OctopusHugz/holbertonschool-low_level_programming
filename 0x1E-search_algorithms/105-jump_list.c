ca5b784b39807eb56a16de09d42108295ba43600
OctopusHugz <colsonscott53@gmail.com>
2020-08-27 20:22:39 +0000

    Changed main loop condition

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -20,7 +20,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		return (NULL);
 	prev_index = list->index;
 	next_index = prev_index + jump;
-	while (/* next_index < size */ list)
+	while (next_index < size /* list */)
 	{
 		node = list;
 		while (node->index < next_index)
@@ -30,7 +30,6 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		{
 			printf("Value found between indexes [%d] and [%d]\n",
 				   (int)prev_index, (int)next_index);
-			/* Start linear search from prev_index to next_index */
 			while (list->index < next_index)
 			{
 				printf("Value checked array[%d] = [%d]\n", (int)list->index,
