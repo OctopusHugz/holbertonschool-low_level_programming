68b6e2766c99655f2891fc2007228c0197eb5cfa
OctopusHugz <colsonscott53@gmail.com>
2020-08-27 20:46:37 +0000

    Added next checks in all while conditions

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -20,17 +20,17 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		return (NULL);
 	prev_index = list->index;
 	next_index = prev_index + jump;
-	while (next_index < size /* list */)
+	while (list && list->next)
 	{
 		node = list;
-		while (node->index < next_index)
+		while (node->index < next_index && node->next)
 			node = node->next;
 		printf("Value checked array[%d] = [%d]\n", (int)next_index, node->n);
 		if (node->n > value)
 		{
 			printf("Value found between indexes [%d] and [%d]\n",
 				   (int)prev_index, (int)next_index);
-			while (list->index < next_index)
+			while (list->index < next_index && list->next)
 			{
 				printf("Value checked array[%d] = [%d]\n", (int)list->index,
 					   list->n);
@@ -39,7 +39,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 				list = list->next;
 			}
 		}
-		while (list->index < next_index)
+		while (list->index < next_index && list->next)
 			list = list->next;
 		prev_index = list->index;
 		next_index = prev_index + jump;
