2b6f61c0ad5f96d97c2924e66131be627d5be271
OctopusHugz <colsonscott53@gmail.com>
2020-08-28 03:02:48 +0000

    Added outline for exercise 13

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - searches for a value in an array of integers using the
+ * linear search algorithm
+ * @list: pointer to the head of the list to search in
+ * @value: value to search for
+ *
+ * Return: first index where value is located, or -1 if head is NULL or value
+ * is not found in array
+ */
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	if (!list)
+		return (NULL);
+}
